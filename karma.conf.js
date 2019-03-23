7082b7f6aaaef02474218f295650aa1f0a3a3ce6
Philipp Huber <Philipp.Huber@capgemini.com>
2019-03-23 20:09:39 +0100

    init for pact

--- karma.conf.js
+++ karma.conf.js
@@ -3,29 +3,40 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: '.',
+    frameworks: ['jasmine', 'pact', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('@pact-foundation/karma-pact'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    flags: ['--no-sandbox'],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    pact: [{
+      port: 1234,
+      consumer: 'ui',
+      provider: 'backend',
+      logLevel: 'DEBUG',
+      log: 'logs/pact.log',
+      dir: 'pacts',
+    }],
+    proxies: {
+      '/api/book': 'http://127.0.0.1:1234/api/book',
+    }
   });
 };
--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Book } from './book';
-import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Book} from './book';
+import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
 
 @Injectable()
 export class BookService {
   private static BOOK_URI = '/api/book';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  findAll(): Observable<Book> {
-    return this.http.get<Book>(BookService.BOOK_URI);
+  findAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(BookService.BOOK_URI);
   }
 
   findOne(id: number): Observable<Book> {
